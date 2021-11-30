853ef4c2982d14eaea1aaa1d7921059e631069b5
An Nguyen Hoang <an2419998@gmail.com>
2021-11-30 17:19:57 +0700

    Change 2 commit

--- app/src/main/java/com/annguyenhoang/gitforandroid/MainActivity.kt
+++ app/src/main/java/com/annguyenhoang/gitforandroid/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
 
         println("This is contained in my second commit")
         println("Local change")
-        println("Experimental")
+        println("Change 2")
     }
 }
