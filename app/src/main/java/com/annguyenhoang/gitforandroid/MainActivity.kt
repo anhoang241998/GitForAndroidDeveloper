e1c19626f8ad5ea9015f7535db40c85f20005290
An Nguyen Hoang <an2419998@gmail.com>
2021-11-30 18:42:41 +0700

    Change 3 commit

--- app/src/main/java/com/annguyenhoang/gitforandroid/MainActivity.kt
+++ app/src/main/java/com/annguyenhoang/gitforandroid/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         println("This is contained in my second commit")
         println("Local change")
         println("Change 2")
+        println("This line will be merged")
     }
 }
